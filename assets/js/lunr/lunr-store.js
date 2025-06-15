87b05ea5c718a5e29ceb33f54e98b5ba1bf59762
utkb23 <utkb23@users.noreply.github.com>
2025-06-15 21:30:33 +0000

    deploy: 74657c7a2c68b213aa055ce0d04e7abef6918289

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,28 @@
 var store = [{
+        "title": "About",
+        "excerpt":"About Me   Write your content here…  ","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Blog",
+        "excerpt":"Blog   Your research details here…  ","categories": [],
+        "tags": [],
+        "url": "/blog/",
+        "teaser": null
+      },{
+        "title": "Publications",
+        "excerpt":"Publications   Your Publication details here…  ","categories": [],
+        "tags": [],
+        "url": "/Publications/",
+        "teaser": null
+      },{
+        "title": "Research",
+        "excerpt":"Research   Your research details here…  ","categories": [],
+        "tags": [],
+        "url": "/research/",
+        "teaser": null
+      },{
         "title": "Post: Modified Date",
         "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
         "tags": ["Post Formats","readability","standard"],
@@ -9,24 +33,6 @@ var store = [{
         "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
         "tags": ["Post Formats","readability","standard"],
         "url": "/blog/2010/01/07/post-standard.html",
-        "teaser": null
-      },{
-        "title": "Post: Chat",
-        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
-        "tags": ["chat","Post Formats"],
-        "url": "/blog/2010/01/08/post-chat.html",
-        "teaser": null
-      },{
-        "title": "Post: Notice",
-        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
-        "tags": ["Post Formats","notice"],
-        "url": "/blog/2010/02/05/post-notice.html",
-        "teaser": null
-      },{
-        "title": "Post: Quote",
-        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
-        "tags": ["Post Formats","quote"],
-        "url": "/blog/2010/02/05/post-quote.html",
         "teaser": null
       },{
         "title": "Post: Link",
